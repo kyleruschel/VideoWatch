7b6c4d186c5856a29d3eda37de697d188d2e67a9
kyleruschel <dkruschel1125@gmail.com>
2020-04-05 00:20:47 -0600

    fixing .gitignore

--- src/Apis/youtube.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const KEY = `AIzaSyDXVpxLJ48sM3fzXn1C4FvFGsDW2tTohpM`;
-
-export default axios.create({
-    baseURL: `https://www.googleapis.com/youtube/v3`,
-});
